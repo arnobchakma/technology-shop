70a35c12994660eec6504642f8d298a973d04d47
arnobchakma <arnobchakma05@gmail.com>
2022-03-11 07:10:44 -0600

    update project again

--- src/Components/AuthConfig/firebaseConfig.js
+++ src/Components/AuthConfig/firebaseConfig.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDI2tpX-tQj1VO4bISbFbvze11mgFa_bVc",
-    authDomain: "technology-shop-1e32b.firebaseapp.com",
-    projectId: "technology-shop-1e32b",
-    storageBucket: "technology-shop-1e32b.appspot.com",
-    messagingSenderId: "963604310955",
-    appId: "1:963604310955:web:6e345716a65d7878bac0ee"
-  };
+  apiKey: "AIzaSyDI2tpX-tQj1VO4bISbFbvze11mgFa_bVc",
+  authDomain: "technology-shop-1e32b.firebaseapp.com",
+  projectId: "technology-shop-1e32b",
+  storageBucket: "technology-shop-1e32b.appspot.com",
+  messagingSenderId: "963604310955",
+  appId: "1:963604310955:web:6e345716a65d7878bac0ee"
+};
 
-  export default firebaseConfig;
+export default firebaseConfig;
